db08dc94c95f6317b1d8575e924b43bbb8910009
Maxence <maxence.rabrais@gmail.com>
2023-04-25 10:08:18 +0200

    Changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-// test
+
 io.on('connection', (socket) => {
   console.log('a user connected' + socket.id);
 });
