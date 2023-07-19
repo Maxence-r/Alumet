916fefc1959d25f97000e90bb4c94005b9831038
Maxence <maxence.rabrais@gmail.com>
2023-07-19 22:33:53 +0200

    Co-authored-by: Gabriel

---
+++ routes/api/alumetGlobal/mailing.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'alumet.io@gmail.com',
+    pass: 'fkplorygttnzrjyq'
+  }
+});
+
+
+
+function sendMail(receiver, subject, content) { // A faire: différent design de mail: 1 pour la confirmation de compte, 1 pour la demande de réinitialisation de mo
+    const mailOptions = {
+        from: 'Alumet.io@gmail.com',
+        to: receiver,
+        subject: subject,
+        html: content
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
+
+sendMail('gabriellancelot1@gmail.com', 'testObject', 'text');
+
+
