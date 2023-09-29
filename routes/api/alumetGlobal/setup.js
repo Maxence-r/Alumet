b911b6dfa9d8236f0dc09e18e562ced54068efff
Maxence <maxence.rabrais@gmail.com>
2023-09-29 11:55:49 +0000

    UI + UX

--- routes/api/alumetGlobal/setup.js
+++ routes/api/alumetGlobal/setup.js
@@ -4,11 +4,18 @@ const path = require('path');
 const authorize = require('../../../middlewares/authentification/authorize');
 
 router.get('/alumet', authorize(), async (req, res) => {
-    console.log(req.user.accountType !== 'staff');
     if (!req.connected || (req.user.accountType !== 'professor' && req.user.accountType !== 'staff')) {
         return res.redirect('/auth/signin');
     }
     const filePath = path.join(__dirname, '../../../views/pages/setup/alumet.html');
+    res.sendFile(filePath);
+});
+
+router.get('/flashcards', authorize(), async (req, res) => {
+    if (!req.connected) {
+        return res.redirect('/auth/signin');
+    }
+    const filePath = path.join(__dirname, '../../../views/pages/setup/flashcards.html');
     res.sendFile(filePath);
 });
 
