5042c430f6d1a63e2fab96e660d349abd9616143
Maxence <maxence.rabrais@gmail.com>
2024-08-06 19:35:40 +0200

    ???????

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get("/", (req, res) => {
 
 // ROLLOUT
 const rolloutExperiment = require("./middlewares/utils/rollout.js");
-rolloutExperiment("disableAlumet", "2024-08-09T01:01:30.000Z");
+rolloutExperiment("disableAlumet", "2024-08-06T01:01:30.000Z");
 
 // Alumet application
 app.use("/portal", portal);
